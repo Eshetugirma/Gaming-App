31892c76b5af891c9aa15a934eff897ff5d65951
Eshetugirma <eshetugirma321@gmail.com>
2024-01-17 18:06:42 +0300

    obstacles and logic added

--- lib/components/pipe_group.dart
+++ lib/components/pipe_group.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:gaming_app/components/pipe.dart';
 import 'package:gaming_app/game/configuration.dart';
@@ -6,12 +8,21 @@ import 'package:gaming_app/game/pipe_position.dart';
 
 class PipeGroup extends PositionComponent with HasGameRef<HomePage> {
   PipeGroup();
+
   @override
   Future<void> onLoad() async {
     position.x = gameRef.size.x;
+    final heightMinusGround = (gameRef.size.y - Config.groundHeight) ~/ 2;
+    final spacing = Random().nextInt(heightMinusGround.toInt());
+    print(spacing);
+
     addAll([
-      Pipe(height: 100, pipePosition: PipePosition.top),
-      Pipe(height: 100, pipePosition: PipePosition.top),
+      Pipe(
+          height: heightMinusGround + spacing - 150,
+          pipePosition: PipePosition.top),
+      Pipe(
+          height: heightMinusGround - spacing - 100,
+          pipePosition: PipePosition.bottom),
     ]);
   }
 
@@ -19,5 +30,8 @@ class PipeGroup extends PositionComponent with HasGameRef<HomePage> {
   void update(dt) {
     super.update(dt);
     position.x -= Config.gameSpeed * dt;
+    if (position.x <= -10) {
+      removeFromParent();
+    }
   }
 }
--- lib/game/gaming_page.dart
+++ lib/game/gaming_page.dart
@@ -1,11 +1,18 @@
+import 'dart:async';
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:gaming_app/components/background.dart';
 import 'package:gaming_app/components/bird.dart';
 import 'package:gaming_app/components/ground.dart';
 import 'package:gaming_app/components/pipe_group.dart';
+import 'package:gaming_app/game/configuration.dart';
 
 class HomePage extends FlameGame {
+  HomePage();
   late Bird bird;
+  Timer interval = Timer(Config.pipeInterval, repeat: true);
+
+  @override
   Future<void> onLoad() async {
     addAll([
       Background(),
@@ -13,5 +20,12 @@ class HomePage extends FlameGame {
       bird = Bird(),
       PipeGroup(),
     ]);
+    interval.onTick = () => add(PipeGroup());
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    interval.update(dt);
   }
 }
